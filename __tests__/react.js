4440cd94ef57b967e9f862d7de3789a4b34e3839
samos17 <amos.serena17@gmail.com>
2022-09-19 14:12:53 -0400

    create boilerplate for react and puppeteer testing

---
+++ __tests__/react.js
@@ -0,0 +1,10 @@
+import React from 'React';
+import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import App from '../client/App';
+
+
+describe('Unit testing React components', () => { 
+  
+})
