3a59e9347863d7c5242112d74b420b38bb812347
samos17 <amos.serena17@gmail.com>
2022-09-20 13:09:16 -0400

    add testing to check if header component renders

--- __tests__/react.js
+++ __tests__/react.js
@@ -1,33 +1,18 @@
 import React from 'React';
-import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { Header } from '../client/components/Header.jsx';
+import {Header} from '../client/components/Header'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+
 
 describe('Unit testing React components', () => {
-  describe('Header', () => {
-    beforeEach(() => {
-      renderedText = render(<Header />);
-    });
-    test('it runs without crashing', () => {
-      render(<Header />);
-    });
-    xtest('Renders dropdown menu', () => {
-      const dropdown = document.getElementById('request-selector');
-      const display = dropdown.children[0];
-      expect(display.textContent).toBe('Get');
-      console.log(display.textContent);
-      fireEvent.click(dropdown);
-      const dropdownOptions = getAllByRole(dropdown, 'option');
-      fireEvent.click(dropdownOptions[2]);
-      expect(display.textContent).toBe('Post');
-      console.log(display.textContent);
-    });
-    xtest('Renders add button', () => {
-      const button = document.getElementById('add-assertion');
-      expect(button).toBeTruthy();
-      fireEvent.click(button);
-      expect('----onclick mock function goes here').toHaveBeenCalled();
-    });
-  });
+  const initialState = {testForm: {requestType: 'Get', assertionList: []}};
+  const mockStore = configureStore()
+  let store
+  test('Header components renders successfully', () => {
+    store = mockStore(initialState);
+    render(<Provider store={store}><Header/></Provider>)
+  })
 });
