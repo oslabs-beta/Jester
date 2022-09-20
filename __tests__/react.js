521910a5af76744f52cac14372c0bcd82631e5a2
samos17 <amos.serena17@gmail.com>
2022-09-20 16:44:25 -0400

    add dependencies for react testing

--- __tests__/react.js
+++ __tests__/react.js
@@ -11,8 +11,40 @@ describe('Unit testing React components', () => {
   const initialState = {testForm: {requestType: 'Get', assertionList: []}};
   const mockStore = configureStore()
   let store
-  test('Header components renders successfully', () => {
+  beforeEach(() => {
     store = mockStore(initialState);
     render(<Provider store={store}><Header/></Provider>)
   })
+  test('Header component renders successfully', () => {
+    store = mockStore(initialState);
+    render(<Provider store={store}><Header/></Provider>)
+  })
+  test('Dropdown menu for request type renders successfully', () => {
+    // const dropdown = screen.getByDisplayValue('Get');
+    // console.log(dropdown)
+    // expect(dropdown.type).toEqual('text');
+    // const dropdown = document.querySelector('#request-selector')
+    expect(screen.getByText('Get')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Get'));
+    fireEvent.click(screen.getByText('Post'));
+    expect(screen.getByText('Post')).toBeInTheDocument();
+
+    //userEvent
+
+    
+    // console.log(screen.getByText('Get').nextSibling)
+
+    // console.log(document.querySelector('#request-selector'))
+
+    
+  })
+  test('Add assertion button renders successfully', () => {
+    expect(screen.getByText('+')).toBeInTheDocument();
+    const addAssertionButton = screen.getByText('+');
+    expect(addAssertionButton.type).toEqual('button');
+  })
+
+  // dropdown menu renders
+  // add button renders
+
 });
