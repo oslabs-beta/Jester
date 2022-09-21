b4b208952527e8eaedc3914fc3cf6a36250eef03
samos17 <amos.serena17@gmail.com>
2022-09-21 16:54:58 -0400

    clean up code and refactor header menu items

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -15,71 +15,54 @@ import { Middle } from './Middle';
 export const Header = () => {
   const requestType = useSelector((state) => state.testForm.requestType);
   const assertionObject = useSelector((state) => state.testForm.assertionList);
-  const index = useSelector((state) => state.testForm.i);
-  // const numOfAssertions = useSelector((state) => state.testForm.numOfAssertions);
-  // console.log('assertion Object: ', assertionObject[0])
-  // const assertionList = [];
-  // const values = []
-  // if (index !== 0) {
-  //   let i = 0;
-  //   while (i < index) {
-  //     if (assertionObject[i]) {
-  //       let el = assertionObject[i]
-  //       assertionList.push(el[0])
-  //     }
-  //     i += 1;
-  //   }
-  // }
   const assertionList = [];
   const assertionIds = Object.keys(assertionObject);
   for (let id of assertionIds) {
-    assertionList.push(<Middle id={id} key={id} />)
-
+    assertionList.push(<Middle id={id} key={id} />);
   }
-
-  // console.log(assertionList)
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Submit Post Request');
   };
+  const handleChange = (e) => dispatch(setRequestType(e.target.value));
+  const handleAdd = () => dispatch(addAssertion());
+
+  const menuItems = [];
+  const menuOptions = ['Get', 'Post', 'Patch', 'Delete'];
+  for (let option of menuOptions) {
+    menuItems.push(<MenuItem key={option} value={option}>{option}</MenuItem>)
+  }
+
   return (
     <form id="test-generator-form" onSubmit={handleSubmit}>
       <span>
-      <FormControl>
-      <InputLabel id="requestSelector">Request Type</InputLabel>
-        <Select
-          name="request-selector"
-          id="request-selector"
-          data-testid="request-selector"
-          label="Request Type"
-          // value={values[index]}
-          onChange={(e) => dispatch(setRequestType(e.target.value))}
-        >
-          <MenuItem key="Get" value="Get">
-            Get
-          </MenuItem>
-          <MenuItem key="Post" data-testid="Post" value="Post">
-            Post
-          </MenuItem>
-          <MenuItem key="Patch" value="Patch">
-            Patch
-          </MenuItem>
-          <MenuItem key="Delete" value="Delete">
-            Delete
-          </MenuItem>
-        </Select>
-      </FormControl>
-      <TextField label="Endpoint" data-testid={requestType} id={requestType} name={requestType} />
+        <FormControl>
+          <InputLabel id="requestSelector">Request Type</InputLabel>
+          <Select
+            name="request-selector"
+            id="request-selector"
+            data-testid="request-selector"
+            label="Request Type"
+            value={requestType}
+            onChange={ handleChange }
+          >
+            {menuItems}
+          </Select>
+        </FormControl>
+        <TextField
+          label="Endpoint"
+          data-testid={requestType}
+          id={requestType}
+          name={requestType}
+        />
       </span>
       <Box id="assertion-list">Assertion List: {assertionList}</Box>
       <Button
         id="add-assertion"
         name="add-assertion"
         variant="contained"
-        onClick={() => {
-          dispatch(addAssertion());
-        }}
+        onClick={ handleAdd }
       >
         +
       </Button>
--- client/redux/reducers/testFormSlice.js
+++ client/redux/reducers/testFormSlice.js
@@ -18,25 +18,14 @@ export const testFormSlice = createSlice({
       state.requestType = action.payload;
     },
     addAssertion: (state) => {
-      // state.assertionList[state.i] = <Middle id={state.i} key={state.assertionList.length} />
-      // console.log(state.assertionList)
-      // state.assertionList.push(<Middle id={state.i} key={state.assertionList.length}  value='Status Code' />)
-      // console.log(state.assertionList[0])
-      // drill id down to button
       state.i += 1;
       state.assertionList[state.i] = 'Status Code';
     },
     setInputType: (state, action) => {
-      console.log(action.payload)
       state.assertionList[action.payload.id] = action.payload.type;
     },
-    addUserInput: (state, action) => {
-      state.userInput = action.payload.value;
-    },
     deleteAssertion: (state, action) => {
-      //action payload will equal e.target.id
       delete state.assertionList[action.payload]
-      // state.assertionList.splice(action.payload, 1);
     },
   },
 });
@@ -45,7 +34,6 @@ export const {
   setRequestType,
   addAssertion,
   setInputType,
-  addUserInput,
   deleteAssertion,
 } = testFormSlice.actions;
 
