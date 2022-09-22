d68904328c04c62d64c3a75e1e5b78f64da65e36
samos17 <amos.serena17@gmail.com>
2022-09-22 15:54:30 -0400

    add text to tutorial component for mvp

--- client/components/Tutorial.jsx
+++ client/components/Tutorial.jsx
@@ -5,22 +5,36 @@ export const Tutorial = () => {
   return (
     <div>
       <Typography variant="h4" component="h2">
-        You will need to create a __tests__ folder in your project with a file
-        called supertest.js. You will also need to add jest and supertest as
-        dependencies in your package.json file.
+        <ol>
+          <li>
+            Create a __tests__ folder in your project with a file called
+            supertest.js
+          </li>
+          <li>
+            Add jest and supertest as dependencies in your package.json file
+          </li>
+          <li>
+            Add the following to your supertest.js file:
+            <ul>
+              <li>
+                <code>const &#123; response &#125; = require('express')</code>
+              </li>
+              <li>
+                <code>const request = require('supertest')</code>
+              </li>
+              <li>
+                <code>
+                  const server = '
+                  <em>
+                    <u>Place server information here</u>
+                  </em>
+                  '
+                </code>
+              </li>
+            </ul>
+          </li>
+        </ol>
       </Typography>
-      <Typography variant="h4" component="h2">
-        You will need to add the following to your supertest.js file:
-      </Typography>
-      <code>
-        const &#123; 'response' &#125; = require('express')<br></br>
-        const request = require('supertest')<br></br>
-        const server = '
-        <em>
-          <u>Place server information here</u>
-        </em>
-        ' <br></br>
-      </code>
     </div>
   );
 };
