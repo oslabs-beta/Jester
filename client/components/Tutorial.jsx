510a21cfa389e0be7a40099b70c29adcdf7ddd69
samos17 <amos.serena17@gmail.com>
2022-09-22 14:03:49 -0400

    add basic tutorial information

--- client/components/Tutorial.jsx
+++ client/components/Tutorial.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Typography } from '@mui/material';
 
 export const Tutorial = () => {
-//   const { response } = require('express');
-// const request = require('supertest');
-// const db = require('../server/db/markets.js');
-
-// const server = 'http://localhost:3000';
-
-}
+  return (
+    <div>
+      <Typography variant="h4" component="h2">
+        You will need to create a __tests__ folder in your project with a file
+        called supertest.js. You will also need to add jest and supertest as
+        dependencies in your package.json file.
+      </Typography>
+      <Typography variant="h4" component="h2">
+        You will need to add the following to your supertest.js file:
+      </Typography>
+      <code>
+        const &#123; 'response' &#125; = require('express')<br></br>
+        const request = require('supertest')<br></br>
+        const server = '
+        <em>
+          <u>Place server information here</u>
+        </em>
+        ' <br></br>
+      </code>
+    </div>
+  );
+};
