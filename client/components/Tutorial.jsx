654af05237f248014be0a0858c3f264bb39e0994
samos17 <amos.serena17@gmail.com>
2022-09-22 13:41:22 -0400

    add tutorial component

---
+++ client/components/Tutorial.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Tutorial = () => {
+//   const { response } = require('express');
+// const request = require('supertest');
+// const db = require('../server/db/markets.js');
+
+// const server = 'http://localhost:3000';
+
+}
