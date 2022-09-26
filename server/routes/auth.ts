9ebb42919d333ff617c2ebe9d75b73d16dcbb24d
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-09-26 16:22:16 -0400

    fix redirect

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -14,12 +14,13 @@ router.get('/github',
   passport.authenticate('github',{ scope: [ 'user:email' ] })
 );
 
-// 
+// route that gets called once the user is authenticated by GitHub
 router.get('/github/callback',
   passport.authenticate('github', { failureRedirect: '/error' }),
   (req: Request, res: Response): void => {
-    console.log('Rerouting  ')
-    return res.redirect('../');
+    // is this the right place to insert logic for SQL db for user table
+    // if user doesn't exist on the database, add to user table
+    return res.redirect('../../');
   }
 );
 
