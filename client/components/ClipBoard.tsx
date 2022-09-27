f46b9952c0aa4ccffe5c671b1e34605ecd55f41e
Anshuman Sinha <anshuman@Anshumans-MacBook-Air.local>
2022-09-27 09:46:52 -0500

    updates to clipboard component. Still needs state slicing functonality

--- client/components/ClipBoard.tsx
+++ client/components/ClipBoard.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 //What import from MUI? Kinda don't want to use a Textfield bc it can be edited and I can't protect it from that?
 import TextField from '@mui/material/TextField';
 //Will need RTK?
-import { useSelector, useDispatch } from 'react-redux'
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { userEditText } from '../redux/reducers/reducer';
+import { ChangeEvent } from 'react';
 
+
+// const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setName(event.target.value);
+//   };
 
 
 
 //Slice of State named to ClipboadData
-//Have a reducer called setClipboard (thi sserena will use to set this clipboard data.) Will need state in action payload and payload will be array of strings from db.
+//Have a reducer called setClipboard (thi sserena will use to set this clipboard data.) Will pass the clipboard text form the db to the action payload and payload will be array of strings from db.
+// access clipboard info from state and put it into the clipboard. Set the clipboard data in state.
+
+function ClipBoard() {
+    const dispatch = useAppDispatch();
+    const editCode = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => dispatch(userEditText(e.target.value));
+    // const setClipboard = ();
 
 
-function ClipBoard(props) {
     return (
         <TextField 
         id="clipboard"
         multiline
         rows={10}
+        defaultValue={ "Your Clipboard is currently empty!" }
         sx={{ 
             width: 0.95,
             fontFamily: "Source Code Pro",
             }}
+        onChange = { editCode }
+
         />
     );
 }
