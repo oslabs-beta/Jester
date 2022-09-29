a32642a7751438ab864e19a83312d66441c397d9
BrianHao <BrianHao@users.noreply.github.com>
2022-09-29 14:54:35 -0400

    login/logout routing

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -4,50 +4,50 @@ import { Express, Request, Response, NextFunction } from 'express';
 import db from '../models/userModel';
 
 type GitHubSettingsType = {
-  clientID: string,
-  clientSecret: string,
-  callbackURL: string,
-  scope: Array<string>,
-}
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
+  scope: Array<string>;
+};
 
 type AuthType = {
-  isLoggedIn: (req: Request, res: Response, next: NextFunction) => void,
-  getUserId: (req: Request, res: Response, next: NextFunction) => void,
-}
-
+  isLoggedIn: (req: Request, res: Response, next: NextFunction) => void;
+  getUserId: (req: Request, res: Response, next: NextFunction) => void;
+};
 
 const gitHubSettings: GitHubSettingsType = {
   clientID: 'fa73697734733fc09ac6',
   clientSecret: '00e08080239a284e4047e1342393f88dc3ada6ae',
   callbackURL: 'http://localhost:3000/auth/github/callback',
-  scope: [ 'user:email' ],
+  scope: ['user:email']
 };
 
 // need to clean up the type declarations of user, done, profile and tokens
 // https://typescript.hotexamples.com/examples/passport/-/serializeUser/typescript-serializeuser-function-examples.html
 // https://stackoverflow.com/questions/65772869/how-do-i-type-hint-the-user-argument-when-calling-passport-serializeuser-in-type
-passport.serializeUser(
-  function(user: Express.User, done: any) {
-    done(null, user);
-  });
+passport.serializeUser(function (user: Express.User, done: any) {
+  done(null, user);
+});
 
-passport.deserializeUser(
-  function(user: any, done: any) {
-    done(null, user);
-  });
+passport.deserializeUser(function (user: any, done: any) {
+  done(null, user);
+});
 
 passport.use(
-  new GitHubStrategy(
-    gitHubSettings,
-    function(accessToken: any, refreshToken: any, profile: any, done: any) {
-      const email: string = profile.emails[0].value;
-      const params = [email];
-      console.log(email);
-      // logic for creating a new record on the database for the user could go in here
-      
-      return done(null, profile);
-    }
-  ));
+  new GitHubStrategy(gitHubSettings, function (
+    accessToken: any,
+    refreshToken: any,
+    profile: any,
+    done: any
+  ) {
+    const email: string = profile.emails[0].value;
+    const params = [email];
+    // console.log(email);
+    // logic for creating a new record on the database for the user could go in here
+
+    return done(null, profile);
+  })
+);
 
 export const authController: AuthType = {
   // Middleware to verify that user is logged in (typically used before database calls)
@@ -59,7 +59,7 @@ export const authController: AuthType = {
     //   return res.status(401).json("Error: User not authorized");
     // }
     // MLCK: Is this a security vulnerability?  Another option is to store session in database and make a db call
-    if(!req.user || !req.isAuthenticated()){
+    if (!req.user || !req.isAuthenticated()) {
       return res.status(401).json('Error: User not authorized');
     }
     // console.log('authController.isLoggedIn');
@@ -83,4 +83,3 @@ export const authController: AuthType = {
 };
 
 export default passport;
-
--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -4,36 +4,46 @@ import passport, { authController } from '../controllers/authController';
 const router: Router = express.Router();
 
 // route checking if user is authorized
-router.get('/', 
-  authController.isLoggedIn, 
+router.get(
+  '/',
+  authController.isLoggedIn,
   authController.getUserId,
   (req, res) => {
     return res.status(200).json(res.locals.userId);
-  });
-
-// route for user being unable to sign in with GitHub
-router.get('/error', 
-  (req: Request, res: Response): Response => {
-    return res.send('Unknown Error');
-  });
-
-// route for dialog box to GitHub authorization (i.e. login with Github)
-router.get('/github',
-  passport.authenticate('github',{ scope: [ 'user:email' ] })
-);
-
-// route that gets called once the user is authenticated by GitHub
-router.get('/github/callback',
-  passport.authenticate('github', { failureRedirect: '/error' }),
-  (req: Request, res: Response): void => {
-    return res.redirect('../../');
   }
 );
 
-router.post('/logout', 
-  (req: any, res: Response, next: NextFunction): void => {
-    req.logout();
-    return res.redirect('../../');
-  });
+// route for user being unable to sign in with GitHub
+router.get('/error', (req: Request, res: Response): Response => {
+  return res.send('Unknown Error');
+});
+
+// route for dialog box to GitHub authorization (i.e. login with Github)
+router.get(
+  '/github',
+  passport.authenticate('github', { scope: ['user:email'] })
+);
+
+// route that gets called once the user is authenticated by GitHub
+router.get(
+  '/github/callback',
+  passport.authenticate('github', { failureRedirect: '/error' }),
+  // Can add middleware to query DB for user id here!
+  // Can add middleware to store auth code+username in sessions table that expires
+  (req: Request, res: Response): void => {
+    // /github/callback/?code=4234324324 <= user authorization code
+    res.cookie('code', req.query.code);
+    res.cookie('email', req.user.emails[0].value);
+    res.cookie('username', req.user.username);
+    res.cookie('isLoggedIn', true);
+    // res.cookie('userId', res.locals.userId) // return userID here
+    return res.redirect('../../authenticate');
+  }
+);
+
+router.post('/logout', (req: any, res: Response, next: NextFunction): void => {
+  req.logout();
+  return res.redirect('../../');
+});
 
 export default router;
