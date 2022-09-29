0e3e6519e24a6ebc52d0e972a92beb28c002b7ea
samos17 <amos.serena17@gmail.com>
2022-09-29 13:17:23 -0400

    fix testUserInfoReducer

--- __tests__/Reducer Unit Testing/testUserInfoReducer.ts
+++ __tests__/Reducer Unit Testing/testUserInfoReducer.ts
@@ -1,18 +1,36 @@
 import { store } from '../../client/redux/store';
 import { setShowLogin } from '../../client/redux/reducers/userInfoSlice';
+import { Z_DEFAULT_STRATEGY } from 'zlib';
 
-type defaultStateType = {
-  showLogin: boolean,
-  isLoggedIn: boolean,
-}
+type projectsType = {
+  project_id: number;
+  project_name: string;
+  user_id: number;
+  clipboardInfo?: string[];
+  showAccessClipboard: boolean;
+};
+
+type userInfoStateType = {
+  showLogin: boolean;
+  isLoggedIn: boolean;
+  userId: number;
+  projectsInfo: projectsType[];
+};
 
 describe('UserInfo Reducer', () => {
-  let defaultState: defaultStateType;
-  beforeEach(() => {
-    defaultState = {
-      showLogin: false,
-      isLoggedIn: false,
-    };
+  let defaultState: userInfoStateType;
+  beforeEach(() => defaultState = {
+    showLogin: false,
+    isLoggedIn: false,
+    userId: 0,
+    projectsInfo: [
+      {
+        project_id: 0,
+        project_name: 'Project One',
+        user_id: 0,
+        showAccessClipboard: false,
+      },
+    ],
   });
   test('it should return a default state', () => {
     const state = store.getState();
