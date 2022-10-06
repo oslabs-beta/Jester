f29db85a7d8d34048dc20316a782796db6db46ad
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-10-06 16:18:47 -0400

    fix boilerplate

--- client/constants/index.js
+++ client/constants/index.js
@@ -6,10 +6,11 @@ export const boilerplate_start = (server) => {
   return [
   "const request = require('supertest');\n",
   `const server = '${server}';\n\n`,
-  "describe('Route Integration Testing'), ( ) => {\n",
+  "describe('Route Integration Testing', ( ) => {\n",
   ];
 }
 
-export const BOILERPLATE_END = '};'
+export const BOILERPLATE_END = '});'
 
 export const DEFAULT_CLIPBOARD= 'Your Clipboard is currently empty! Please generate a test before we can display your testing code here.';
+
