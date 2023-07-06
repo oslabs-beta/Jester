7eb5a5ffeb3bb4d6f5f6a4a774e6f9696a8799ea
Anshuman Sinha <anshumansinh@gmail.com>
2023-07-06 00:10:49 -0500

    feat: first commit to initialize this dummy Jester application

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,93 +1,117 @@
+// import { NextFunction, Request, Response } from 'express';
+// import passport from 'passport';
+// import { Profile, Strategy as GitHubStrategy } from 'passport-github2';
+
+// import db from '../models/userModel';
+
+// type GitHubSettingsType = {
+//   clientID: string ;
+//   clientSecret: string ;
+//   callbackURL: string;
+//   scope: Array<string>;
+// };
+
+// type AuthType = {
+//   isLoggedIn: (req: Request, res: Response, next: NextFunction) => void;
+//   getUserId: (req: Request, res: Response, next: NextFunction) => void;
+// };
+
+// type DoneType = (err: Error | null, user: Express.User) => void
+
+// const gitHubSettings: GitHubSettingsType = {
+//   clientID: process.env.clientIDLocal || '',
+//   clientSecret: process.env.clientSecretLocal || '',
+//   callbackURL: `${process.env.root_url}/auth/github/callback`,
+//   scope: ['user:email']
+// };
+
+// passport.serializeUser(function (user: Express.User, done: DoneType) {
+//   done(null, user);
+// });
+
+// passport.deserializeUser(function (user: Express.User, done: DoneType) {
+//   done(null, user);
+// });
+
+// passport.use(
+//   new GitHubStrategy(gitHubSettings, function (
+//     accessToken: string,
+//     refreshToken: string,
+//     profile: Profile,
+//     done: DoneType,
+//   ) {
+//     return done(null, profile);
+    
+//   })
+// );
+
+// export const authController: AuthType = {
+//   // Middleware to verify that user is logged in (typically used before database calls)
+//   isLoggedIn: (req: Request, res: Response, next: NextFunction) => {
+//     if (!req.user || !req.isAuthenticated()) {
+//       return res.status(401).json('Error: User not authorized');
+//     }
+//     return next();
+//   },
+
+//   /*
+//     This controller queries the user table to insert a new user
+//     using the email provided on the request object. If the user
+//     already exists, it performs a mock-update so that regardless
+//     of whether the user already existed or not, it returns the user ID.
+//   */
+//   getUserId: async (req: any, res: Response, next: NextFunction) => {
+//     const email = req.user.emails[0].value;
+//     if (!email) return next({
+//       log: 'email not fond on request body',
+//       status: 400,
+//       message: 'an error occurred in attempting to get the user ID'
+//     });
+//     const newUserQuery = `
+//       INSERT INTO user_table(usermail)
+//       VALUES($1)
+//       ON CONFLICT (usermail) DO UPDATE
+//       SET usermail=($1)
+//       RETURNING user_id
+//       `;
+//     const params = [email];
+//     try {
+//       const result = await db.query(newUserQuery, params);
+//       res.locals.user_id = result.rows[0].user_id;
+//       return next();
+//     }
+//     catch(err) {
+//       return next({
+//         log: `error in getUserId: ${err}`,
+//         status: 500,
+//         message: 'error occurred in getUserId middleware function'
+//       });
+//     }
+//   }
+// };
+
+// export default passport;
+
+
 import { NextFunction, Request, Response } from 'express';
-import passport from 'passport';
-import { Profile, Strategy as GitHubStrategy } from 'passport-github2';
-
-import db from '../models/userModel';
-
-type GitHubSettingsType = {
-  clientID: string ;
-  clientSecret: string ;
-  callbackURL: string;
-  scope: Array<string>;
-};
 
 type AuthType = {
   isLoggedIn: (req: Request, res: Response, next: NextFunction) => void;
   getUserId: (req: Request, res: Response, next: NextFunction) => void;
 };
 
-type DoneType = (err: Error | null, user: Express.User) => void
-
-const gitHubSettings: GitHubSettingsType = {
-  clientID: process.env.clientIDLocal || '',
-  clientSecret: process.env.clientSecretLocal || '',
-  callbackURL: `${process.env.root_url}/auth/github/callback`,
-  scope: ['user:email']
-};
-
-passport.serializeUser(function (user: Express.User, done: DoneType) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user: Express.User, done: DoneType) {
-  done(null, user);
-});
-
-passport.use(
-  new GitHubStrategy(gitHubSettings, function (
-    accessToken: string,
-    refreshToken: string,
-    profile: Profile,
-    done: DoneType,
-  ) {
-    return done(null, profile);
-    
-  })
-);
-
 export const authController: AuthType = {
-  // Middleware to verify that user is logged in (typically used before database calls)
   isLoggedIn: (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user || !req.isAuthenticated()) {
-      return res.status(401).json('Error: User not authorized');
-    }
+    // Bypass authentication and assume user is logged in
+    req.user = { id: 'dummyUserId' };
     return next();
   },
 
-  /*
-    This controller queries the user table to insert a new user
-    using the email provided on the request object. If the user
-    already exists, it performs a mock-update so that regardless
-    of whether the user already existed or not, it returns the user ID.
-  */
   getUserId: async (req: any, res: Response, next: NextFunction) => {
-    const email = req.user.emails[0].value;
-    if (!email) return next({
-      log: 'email not fond on request body',
-      status: 400,
-      message: 'an error occurred in attempting to get the user ID'
-    });
-    const newUserQuery = `
-      INSERT INTO user_table(usermail)
-      VALUES($1)
-      ON CONFLICT (usermail) DO UPDATE
-      SET usermail=($1)
-      RETURNING user_id
-      `;
-    const params = [email];
-    try {
-      const result = await db.query(newUserQuery, params);
-      res.locals.user_id = result.rows[0].user_id;
-      return next();
-    }
-    catch(err) {
-      return next({
-        log: `error in getUserId: ${err}`,
-        status: 500,
-        message: 'error occurred in getUserId middleware function'
-      });
-    }
-  }
+    // Bypass querying user table and assume user ID is 'dummyUserId'
+    res.locals.user_id = 'dummyUserId';
+    return next();
+  },
 };
 
-export default passport;
+export default null;
--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,60 +1,78 @@
-// Authorization routes
-import express, { NextFunction, Request, Response, Router } from 'express';
+// // Authorization routes
+// import express, { NextFunction, Request, Response, Router } from 'express';
 
-import passport, { authController } from '../controllers/authController';
+// import passport, { authController } from '../controllers/authController';
 
-const router: Router = express.Router();
+// const router: Router = express.Router();
 
-/* 
-  This route will check if user is authorized and return their user_id if so
- */
-router.get(
-  '/',
-  authController.isLoggedIn,
-  authController.getUserId,
-  (req, res) => {
-    return res.status(200).json(res.locals.userId);
-  }
-);
+// /* 
+//   This route will check if user is authorized and return their user_id if so
+//  */
+// router.get(
+//   '/',
+//   authController.isLoggedIn,
+//   authController.getUserId,
+//   (req, res) => {
+//     return res.status(200).json(res.locals.userId);
+//   }
+// );
 
-/* 
-  This route will handle user being unable to sign in with GitHub
- */
-router.get('/error', (req: Request, res: Response): Response => {
-  return res.send('Unknown Error');
-});
+// /* 
+//   This route will handle user being unable to sign in with GitHub
+//  */
+// router.get('/error', (req: Request, res: Response): Response => {
+//   return res.send('Unknown Error');
+// });
 
-/* 
-  This route will handle the dialog box to GitHub authorization (i.e. login with Github)
- */
-router.get(
-  '/github',
-  passport.authenticate('github', { scope: ['user:email'] })
-);
+// /* 
+//   This route will handle the dialog box to GitHub authorization (i.e. login with Github)
+//  */
+// router.get(
+//   '/github',
+//   passport.authenticate('github', { scope: ['user:email'] })
+// );
 
-/* 
-  This route will be called called once the user is authenticated by GitHub.
-  Once authenticated, it will save relevant user information to cookies
- */
-router.get(
+// /* 
+//   This route will be called called once the user is authenticated by GitHub.
+//   Once authenticated, it will save relevant user information to cookies
+//  */
+// router.get(
+//   '/github/callback',
+//   passport.authenticate('github', { failureRedirect: '/error' }),
+//   authController.getUserId,
+//   (req: any, res: Response): void => {
+//     res.cookie('code', req.query.code);
+//     res.cookie('email', req.user?.emails[0].value);
+//     res.cookie('username', req.user?.username);
+//     res.cookie('isLoggedIn', true);
+//     return res.redirect('../../authenticate');
+//   }
+// );
+
+// /* 
+//   This route will handle user logout
+//  */
+// router.post('/logout', (req: any, res: Response, next: NextFunction): void => {
+//   req.logout();
+//   return res.redirect('../../');
+// });
+
+// export default router;
+
+import { Router } from 'express';
+import passport from 'passport';
+
+const authRouter = Router();
+
+authRouter.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+authRouter.get(
   '/github/callback',
   passport.authenticate('github', { failureRedirect: '/error' }),
-  authController.getUserId,
-  (req: any, res: Response): void => {
-    res.cookie('code', req.query.code);
-    res.cookie('email', req.user?.emails[0].value);
-    res.cookie('username', req.user?.username);
-    res.cookie('isLoggedIn', true);
-    return res.redirect('../../authenticate');
+  (req, res) => {
+    // Handle successful authentication
+    res.redirect('/success');
   }
 );
 
-/* 
-  This route will handle user logout
- */
-router.post('/logout', (req: any, res: Response, next: NextFunction): void => {
-  req.logout();
-  return res.redirect('../../');
-});
-
-export default router;
+export default authRouter;
